54b03b4d30c84e0ccd50b2b22f6249d9c2e3963f
Jason S. Robinson <jasonsrobinson@protonmail.com>
2024-03-01 13:29:22 -0500

    fix: Removed test-fixtures
    Fixtures were poorly implemented, resulting in free variables being created without closures.
    When the project is loaded from scratch, it would complain about test-data being an unknow symbol.
    
    All (unit) tests passing with project loaded anew.

--- test/corgi_cover/core_test.clj
+++ test/corgi_cover/core_test.clj
@@ -2,24 +2,17 @@
   (:require [clojure.test :refer :all]
             [corgi-cover.core :refer :all]))
 
-(use-fixtures :once
-  (fn setup-test-data [tests]
-    (let [valid-test-states #{"IL" "WA" "NY" "CO"}
-          test-data [{:name "Chloe" :state "IL" :corgi-count 1 :policy-count 0}
-                     {:name "Ethan" :state "IL" :corgi-count 4 :policy-count 2}
-                     {:name "Annabelle" :state "WY" :corgi-count 19 :policy-count 0}
-                     {:name "Logan" :state "WA" :corgi-count 2 :policy-count 1}]
-          test-policies {"Chloe" ["secure goldfish"]
-                         "Ethan" ["cool cats cover" "megasafe"]}]
-      (tests))))
-
 (deftest eligible-test
   (testing "Corgi cover eligibility"
-    (doseq [td test-data]
-      (let [{s :state c :corgi-count} td]
-        (if (valid-test-states s)
-          (is (eligible? s c))
-          (is (not (eligible? s c))))))))
+    (let [test-data [{:name "Chloe" :state "IL" :corgi-count 1 :policy-count 0}
+                     {:name "Ethan" :state "IL" :corgi-count 4 :policy-count 2}
+                     {:name "Annabelle" :state "WY" :corgi-count 19 :policy-count 0}
+                     {:name "Logan" :state "WA" :corgi-count 2 :policy-count 1}]]
+      (doseq [td test-data]
+        (let [{s :state c :corgi-count} td]
+          (if (state s)
+            (is (eligible? s c))
+            (is (not (eligible? s c)))))))))
 
 (deftest tier-coverage-test
   (testing "Returns the correct tier"
@@ -31,14 +24,24 @@
 
 (deftest register-test
   (testing "Returns the correct tier based on application"
-    (is (= :silver   (register (get test-data 0))))
-    (is (= :platinum (register (test-data 1))))
-    (is (= :none     (register (test-data 2))))
-    (is (= :silver   (register (test-data 3))))))
+    (let [test-data [{:name "Chloe" :state "IL" :corgi-count 1 :policy-count 0}
+                     {:name "Ethan" :state "IL" :corgi-count 4 :policy-count 2}
+                     {:name "Annabelle" :state "WY" :corgi-count 19 :policy-count 0}
+                     {:name "Logan" :state "WA" :corgi-count 2 :policy-count 1}]]
+      (is (= :silver   (register (get test-data 0))))
+      (is (= :platinum (register (test-data 1))))
+      (is (= :none     (register (test-data 2))))
+      (is (= :silver   (register (test-data 3)))))))
 
 (deftest registration-test
   (testing "Returns the correct tier based on application"
-    (is (= :silver   (registration (get test-data 0) test-policies)))
-    (is (= :platinum (registration (test-data 1) test-policies)))
-    (is (= :none     (registration (test-data 2) test-policies)))
-    (is (= :silver   (registration (test-data 3) test-policies)))))
+    (let [test-data [{:name "Chloe" :state "IL" :corgi-count 1 :policy-count 0}
+                     {:name "Ethan" :state "IL" :corgi-count 4 :policy-count 2}
+                     {:name "Annabelle" :state "WY" :corgi-count 19 :policy-count 0}
+                     {:name "Logan" :state "WA" :corgi-count 2 :policy-count 1}]
+          test-policies {"Chloe" ["secure goldfish"]
+                         "Ethan" ["cool cats cover" "megasafe"]}]
+      (is (= :silver   (registration (get test-data 0) test-policies)))
+      (is (= :platinum (registration (test-data 1) test-policies)))
+      (is (= :none     (registration (test-data 2) test-policies)))
+      (is (= :silver   (registration (test-data 3) test-policies))))))
